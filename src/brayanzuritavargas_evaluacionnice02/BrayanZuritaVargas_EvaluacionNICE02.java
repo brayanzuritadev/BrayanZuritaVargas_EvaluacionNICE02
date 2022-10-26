3715bdc55c454c2972b91ad9841278959d8e123b
Brayan <brayanzuritavargas@gmail.com>
2022-10-26 15:32:56 -0400

    evaluacion final

--- src/brayanzuritavargas_evaluacionnice02/BrayanZuritaVargas_EvaluacionNICE02.java
+++ src/brayanzuritavargas_evaluacionnice02/BrayanZuritaVargas_EvaluacionNICE02.java
@@ -1,20 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package brayanzuritavargas_evaluacionnice02;
 
-/**
- *
- * @author Brayan
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class BrayanZuritaVargas_EvaluacionNICE02 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        invertirTexto();
+        
     }
     
+    
+    
+    ///pregunta 5: invertir texto
+    private static void invertirTexto(){
+        //prueba
+        String texto = "pedro domingo sabio es el mas sabio";
+        
+        String[] textoDividido= texto.split(" ");
+        String resultado = "";
+        for(int i = 0 ; i<textoDividido.length;i++){
+             if(textoDividido[i].length()>4){
+                 resultado += invertirFragmentoTexto(textoDividido[i])+" ";
+             }else{
+                resultado += textoDividido[i]+" ";
+             }
+        }
+        System.out.println(resultado);
+    }
+    
+    private static String invertirFragmentoTexto(String texto){
+        List<String> caracteresList = new ArrayList<>();
+        String[] caracteres=texto.split("");
+        for(int i = caracteres.length-1; i >= 0;i--){
+            caracteresList.add(caracteres[i]);
+        }
+        return String.join("",caracteresList);
+    }
 }
