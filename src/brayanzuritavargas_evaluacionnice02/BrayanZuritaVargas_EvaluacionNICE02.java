7bcf08627f26d3fab3e2d3d04266db4245ed1137
Brayan <brayanzuritavargas@gmail.com>
2022-10-26 07:20:29 -0400

    Evalucion nice 02 first commit

---
+++ src/brayanzuritavargas_evaluacionnice02/BrayanZuritaVargas_EvaluacionNICE02.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package brayanzuritavargas_evaluacionnice02;
+
+/**
+ *
+ * @author Brayan
+ */
+public class BrayanZuritaVargas_EvaluacionNICE02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
